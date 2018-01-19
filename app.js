e2ca569e7ed1efb5b89419cfcb007a9e400e6ca9
DavidWei95036593 <dw36593@pausd.us>
2018-01-19 15:23:44 -0800

    Fix the bugs

--- app.js
+++ app.js
@@ -1,41 +1,43 @@
 const argv = require("yargs").argv;
 const notes = require("./note.js");
 const command = argv._[0]
+
   if(command ==="add"){
     const title = argv.title;
     const body = argv.body;
-    notes.addNote(title,body);
+    // notes.addNote(title,body);
     const noteAdded = notes.addNote(title,body);
-    if(noteAdded){
+      if(noteAdded){
       console.log("Your note was added");
     }
     else{
-      // console.log(noteAdded);
       console.log("This note already exists");
     }
-    console.log("Add function chosen");
   }
   if(command ==="list"){
     const title = argv.title;
     const body = argv.body;
     notes.listNote(title,body);
-
-    console.log("List function chosen");
   }
   if(command ==="read"){
     const title = argv.title;
     const body = argv.body;
-    notes.readNote(title);
-    console.log("Read function chosen");
+    const noderead = notes.readNote(title);
+    if(noderead){
+
+    }
+    else{
+      console.log("no such node exists");
+    }
   }
   if(command ==="remove"){
     const title = argv.title;
     const body = argv.body;
-    notes.removeNote(title);
     const nodeRemoved = notes.removeNote(title);
     if(nodeRemoved){
-      console.log("Your node is removed");
+    console.log("Your node is removed");
     }
-    else{console.log("no such node exists");}
-    console.log("Remove function chosen");
+    else{
+    console.log("no such node exists");}
+
   }
--- note.js
+++ note.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-
 const fetchNotes = () => {
   try {
     const notesString = fs.readFileSync("notes-data.json");
@@ -12,26 +11,32 @@ const fetchNotes = () => {
 const saveNotes = (notes) => {
   fs.writeFileSync("notes-data.json", JSON.stringify(notes));
 }
-
+//this is a helper method that check to see if the note exists
+const checkNote = (notes,note)=>{
+  if(notes.every(n=> n.title !=note.title)){
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+//this is the add note function that adds the node
 const addNote = (title, body) => {
   const note = {
     title,
     body
-    // title: title,
-    // body: body
   }
   const notes = fetchNotes();
-  if (notes.every(n => n.title != note.title)){
+  if (checkNote(notes,note)){
     notes.push(note);
     saveNotes(notes);
-    console.log("your node was added");
-    return notes;
-
+    return true;
   } else {
-    console.log("This node already exists");
     return false;
   }
 }
+//this is the removenote function that remove the note
 const removeNote = (title)=>{
   const note = {
     title
@@ -39,50 +44,45 @@ const removeNote = (title)=>{
 
   const notes = fetchNotes();
 
-  if (notes.every (n=> n.title == note.title)){
+  if (checkNote(notes,note)==false){
     notes.splice(note.title);
     saveNotes(notes);
     return note;
 
   }
     else{
-      // console.log(notes.every(n=> n.title));
-      // console.log(notes);
-      // console.log("your input title, " + note.title + ", dose not match any of the title that's stored");
-      return false;}
+
+      return false;
+    }
 }
 
-
+//this function will read the note out if there is a note
 const readNote = (title)=>{
     const notes = fetchNotes();
   const note={
-    title
+    title,
   }
-  if (notes.every (n=> n.title == note.title)){
-      console.log(note.title);
-      return notes;
+  if (checkNote(notes,note)==false){
+    console.log(note);
+      return true;
   }
   else
   {
     return false;
-    // console.log(note);
-    // console.log("this title does not exist");
   }
 }
 
-const listNote = (title,body)=>{
-    const notes = fetchNotes();
-  const note ={
-    title,
-    body
+const listNote = () => {
+  const notes = fetchNotes();
+  console.log("Title                    Body");
+  for (i in notes) {
+    let space = " ".repeat(25-notes[i].title.length);
+    console.log(notes[i].title + space + notes[i].body);
   }
-  notes.every(n=>n.title ==note.title);
-  for (var i = 0; i < notes.length; i++) {
-    console.log(notes[i]);
-  }
-
-
 }
+
+
+
 module.exports = {
   addNote,
   removeNote,
